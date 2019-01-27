4c041b7ca4614a0bdd36ba284ed4831708b3bc36
jluzhangyan@163.com <jluzhangyan@163.com>
2019-01-27 11:27:27 +0800

    礼物添加master区分

--- addons/tyzm_diamondvote/inc/web/floatgift.inc.php
+++ addons/tyzm_diamondvote/inc/web/floatgift.inc.php
@@ -8,10 +8,12 @@ if($op=='display'){
 	$pindex = max(1, intval($_GPC['page']));
 	$psize = 20;
 	$condition="";
+	if($_W['username'] != "金鼎文化传播") {
+        $condition="AND a.master = '{$_W['username']}'";
+    }
 	
-	$list = pdo_fetchall("SELECT * FROM ".tablename($this->tablegift)." WHERE uniacid = '{$uniacid} ' ORDER BY id DESC LIMIT 20");
-	
-	
+	$list = pdo_fetchall("SELECT * FROM ".tablename($this->tablegift)." AS u LEFT JOIN ".tablename($this->tablereply)." a ON u.rid = a.rid WHERE u.uniacid = '{$uniacid}' ".$condition." ORDER BY u.id DESC LIMIT 20");
+
 	foreach($list as &$val){
 		$val['ispay']==1?($val['status_cn']="已支付").($val['status_style']='success'):($val['status_cn']="未支付").($val['status_style']='danger');
 		$val['rtitle'] = pdo_fetchcolumn('SELECT title FROM ' . tablename($this->tablereply) . " WHERE uniacid = '{$uniacid}' AND rid = '".$val['rid']."'");
